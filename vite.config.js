919fa4ffe04b0ad4483e91d62d9092e65eff9897
vpro_studio <ti5@tut.by>
2025-04-29 09:52:52 +0300

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-
+  base: '/RPG-Home-ForHost/',
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
@@ -10,6 +10,7 @@ export default defineConfig({
   },
   build: {
     commonjsOptions: {
+      outDir: 'dist',
       include: [/node_modules/],
     }
   },
