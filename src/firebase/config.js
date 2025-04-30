833bc551ce5f5f129c34b5c94ba99c0bf378df6e
vpro_studio <ti5@tut.by>
2025-04-30 14:40:15 +0300

    ahhh update 2

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 // import { initializeApp } from 'firebase/app';
 // import { getFirestore } from 'firebase/firestore';
-import { initializeApp } from '/firebase/app';
-import { getFirestore } from '/firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
